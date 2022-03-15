a855badc99edd1d6bb213b25c0a38b6bcf636c66
Danilo Mancini <haomakk.himself@gmail.com>
2022-03-14 22:05:23 -0300

    Adicionar projeto

---
+++ Raycast-aula1/Assets/Raycasting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycasting : MonoBehaviour
+{
+    public LayerMask mask;
+
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hitInfo; 
+        
+        if (Physics.Raycast(ray, out hitInfo, 100, mask, QueryTriggerInteraction.Ignore))
+        {
+            Debug.DrawLine(ray.origin, hitInfo.point, Color.red);
+            // Esse método desenha uma linha de debug entre o ponto de origem e ponto final do ray.
+            // Quando o ray colide com um objeto, a linha fica vermelha.
+        }
+        else
+        {
+            Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.green);
+            // Enquanto o ray não colide com um objeto, a linha é verde.
+            // Ela se estende até 100 unidades além do ponto de origem.
+        }
+
+    }
+
+}
